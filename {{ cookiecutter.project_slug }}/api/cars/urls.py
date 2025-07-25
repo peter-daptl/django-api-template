6fda5ba2dcbe4baba2a0c883f451facec0ec51a4
Peter Smith <peter@daptl.com>
2025-07-25 06:54:40 -0300

    remove unneeded files

--- {{ cookiecutter.project_slug }}/api/cars/urls.py
+++
@@ -1,8 +0,0 @@
-# In your generated project, open `{{ cookiecutter.project_slug }}/api/cars/urls.py`
-# Replace its entire content with the following (it will be much simpler):
-
-# This file no longer needs to define a router or urlpatterns.
-# It simply serves as a module for the CarViewSet, which is imported by api/urls.py.
-# from rest_framework.routers import DefaultRouter # This line is no longer needed
-from .views import CarViewSet  # Keep this import, as api/urls.py needs it
-# No router or urlpatterns defined here anymore
